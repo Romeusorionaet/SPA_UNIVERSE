db2a30586be0f05dc76fce2f1aefe678b0408f00
romeusorionaet <romeusoares14569@gmail.com>
2022-11-18 15:07:41 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { Router } from './route.js'
+
+const router = new Router()
+
+router.add("/", "/pages/home.html")
+router.add("/universo", "/pages/universo.html")
+router.add("/exploracao", "/pages/exploracao.html")
+router.add(404, "/pages/404.html")
+
+router.handle()
+
+window.onpopstate = () => router.handle()
+window.route = () => router.route()
---
+++ readme.md
@@ -0,0 +1,1 @@
+[link](https://romeusorionaet.github.io/SPA_UNIVERSE/)
